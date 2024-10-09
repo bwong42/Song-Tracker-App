95a7b51cdea6ca9762c195e41dfc70cb3006c8f3
Benjamin Wong <benjamin02615@gmail.com>
2024-10-09 15:01:21 -0700

    Added learning field

--- src/main/model/Song.java
+++ src/main/model/Song.java
@@ -5,6 +5,7 @@ public class Song {
     private String title; // title of song
     private String artist; // artist of song
     private String instrument; // instrument of song to learn
+    private Boolean learning; // currently learning song?
     private Boolean learned; // is song learned or not?
     private Boolean favourite; // is song favourited?
     
@@ -12,13 +13,15 @@ public class Song {
      * REQUIRES: title has non-zero length, artist has non-zero length,
      * instrument has non-zero length,
      * EFFECTS: title of song set to title, artist of song set to artist,
-     * instrument of song set to instrument, learned initially set to false,
-     * favourite initially set to false
+     * instrument of song set to instrument, learning initially set to false,
+     * learned initially set to false, favourite initially set to false.
      */
     public Song(String title, String artist, String instrument){
         this.title = title;
         this.artist = artist;
         this.instrument = instrument;
+
+        this.learning = false;
         this.learned = false;
         this.favourite = false;
     }
@@ -35,12 +38,20 @@ public class Song {
         return instrument;
     }
 
+    public Boolean isLearning() {
+        return learning;
+    }
+
     public Boolean isLearned() {
         return learned;
     }
 
-    public Boolean favourite() {
+    public Boolean isFavourite() {
         return favourite;
+    }
+
+    public Boolean makeLearning(){
+        return learning = true;
     }
 
     public Boolean makeLearned(){
