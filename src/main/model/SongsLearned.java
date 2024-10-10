dca33a8493713ad0f2fcfb52dad734f9bf220103
Benjamin Wong <benjamin02615@gmail.com>
2024-10-09 23:33:59 -0700

    Deleted unneccesary list import

--- src/main/model/SongsLearned.java
+++ src/main/model/SongsLearned.java
@@ -1,7 +1,6 @@
 package model;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class SongsLearned {
     private ArrayList<Song> songsLearned;
--- src/main/model/SongsToLearn.java
+++ src/main/model/SongsToLearn.java
@@ -1,7 +1,6 @@
 package model;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class SongsToLearn {
     private ArrayList<Song> songsLearning;
