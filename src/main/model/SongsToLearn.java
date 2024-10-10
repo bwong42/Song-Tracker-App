db79e40461143d1e3b46a126fa0221389c1cba27
Benjamin Wong <benjamin02615@gmail.com>
2024-10-09 17:47:16 -0700

    Specification for SongsToLearn

---
+++ src/main/model/SongsToLearn.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SongsToLearn {
+    private ArrayList<Song> songsLearning;
+
+    /** 
+     * EFFECTS: SongsToLearn is initially set to empty.
+     */
+    public SongsToLearn(){
+        songsLearning = new ArrayList<>();
+
+        }
+
+    //EFFECTS: Adds song to SongsToLearn
+    public void addSongToSongsToLearn(Song song){
+        //STUB
+    }
+
+    //EFFECTS: Removes song from SongsToLearn
+    public void removeSongToSongsToLearn(Song song){
+        //STUB
+    }
+
+    public ArrayList<Song> getSongs(){
+        //STUB
+    }
+
+    }
+
+
