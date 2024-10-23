d55ecaca99039b1c43e2ad7033edb9d15b7e69f6
Benjamin Wong <benjamin02615@gmail.com>
2024-10-22 23:06:41 -0700

    Specification for toJson and songsToJson methods in SongsLearning Class

--- src/main/model/SongsLearning.java
+++ src/main/model/SongsLearning.java
@@ -2,6 +2,9 @@ package model;
 
 import java.util.ArrayList;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 // Represents an list of songs called songsLearning
 public class SongsLearning {
     private ArrayList<Song> songsLearning; // An array list of song
@@ -31,5 +34,23 @@ public class SongsLearning {
 
     public ArrayList<Song> getSongs() {
         return this.songsLearning;
+    }
+
+    // EFFECTS: creates a new JSONObject and puts songs in it
+    public JSONObject toJson() {
+        JSONObject json = new JSONObject();
+        json.put("songs", songsToJson());
+        return json;
+    }
+
+    // EFFECTS: returns things in this workroom as a JSON array
+    private JSONArray songsToJson() {
+        JSONArray jsonArray = new JSONArray();
+
+        for (Song song: songsLearning) {
+            jsonArray.put(song.toJson());
+        }
+
+        return jsonArray;
     }
 }
